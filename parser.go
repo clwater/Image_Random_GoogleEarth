cd31673e9ba9067b84297957d8f9a4d878f54ce1
clwater <gzb@clwater.top>
2021-11-25 09:20:52 +0800

    WIP(parser): 测试

--- parser.go
+++ parser.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"log"
+	"os"
+	"strings"
 )
 
 type GoogleEarth struct {
@@ -15,7 +18,7 @@ type GoogleEarth struct {
 var infos []GoogleEarth
 
 func main() {
-	index := 1000
+	index := 2000
 	failIndex := 0
 
 	for ;failIndex < 100 ;  {
@@ -24,19 +27,51 @@ func main() {
 
 		client := resty.New()
 		resp, err := client.R().Get(url)
-		if err != nil || resp.StatusCode() == 200 {
+		//多维度判断页面内容是否有效
+		if err != nil || resp.StatusCode() != 200 || resp.Size() < 2000{
 			failIndex++
 		}else {
 			failIndex = 0
 			infos = append(infos, GoogleEarth{index, url})
-
 		}
 		index++
+
+		//每检索10此进行一次保存
+		if index % 10 == 0 {
+			//log.Println(infos)
+			j, _ := json.Marshal(infos)
+			log.Println(string(j))
+			Save(string(j))
+			infos = nil
+		}
+
 	}
 	
-	//log.Println(infos)
 
-	j, _ := json.Marshal(infos)
-	log.Println(string(j))
 
 }
+
+
+//
+//  Save
+//  @Description: 追加相应内容到文件中
+//  @param text
+//
+func Save(text string){
+	var filename = "earthview.json"
+	var f *os.File
+	var err1 error
+	f, err1 = os.OpenFile(filename, os.O_APPEND, 0666) //打开文件
+	defer f.Close()
+	if err1 != nil {
+		panic(err1)
+	}
+	w := bufio.NewWriter(f) //创建新的 Writer 对象
+	text = strings.ReplaceAll(text, "[", "")
+	text = strings.ReplaceAll(text, "]", ",")
+	text = text + "\n"
+	_, _ = w.WriteString(text)
+	//fmt.Printf("写入 %d 个字节n", n)
+	w.Flush()
+}
+
